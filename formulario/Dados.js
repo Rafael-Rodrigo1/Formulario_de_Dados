ab5b6c876139549dde3bbc499cb123e0e0116b11
Rafael-Rodrigo1 <161649414+Rafael-Rodrigo1@users.noreply.github.com>
2024-03-29 23:46:05 -0300

    criei 2 arquivo

---
+++ formulario/Dados.js
@@ -0,0 +1,13 @@
+function clique() {
+    const sen1 = document.getElementById('senha1')
+    const sen2 = document.getElementById('senha2')
+    
+    if (sen2.value === sen1.value) {
+      sen2.setCustomValidity('')
+    } else {
+      sen2.setCustomValidity('senha diferem')
+      }
+    }
+    function clicar() {
+    alert('Registro realizado com sucesso')
+    }
